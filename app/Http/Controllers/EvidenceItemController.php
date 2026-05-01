55b1f2167bbb2d2fe5daec64c9b1767b7bf68454
Bafageeh <a.baf@live.com>
2026-05-02 00:54:06 +0300

    Sort evaluation criteria by leading number

--- app/Http/Controllers/EvidenceItemController.php
+++ app/Http/Controllers/EvidenceItemController.php
@@ -20,12 +20,49 @@ class EvidenceItemController extends Controller
         return EvidenceItem::where('school_id', Auth::user()->school_id)->findOrFail($id);
     }
 
+    private function evidenceOrderNumber(EvidenceItem $item): int
+    {
+        $title = strtr($item->title, [
+            '٠' => '0',
+            '١' => '1',
+            '٢' => '2',
+            '٣' => '3',
+            '٤' => '4',
+            '٥' => '5',
+            '٦' => '6',
+            '٧' => '7',
+            '٨' => '8',
+            '٩' => '9',
+            '۰' => '0',
+            '۱' => '1',
+            '۲' => '2',
+            '۳' => '3',
+            '۴' => '4',
+            '۵' => '5',
+            '۶' => '6',
+            '۷' => '7',
+            '۸' => '8',
+            '۹' => '9',
+        ]);
+
+        if (preg_match('/^\s*(\d+)/u', $title, $matches)) {
+            return (int) $matches[1];
+        }
+
+        return PHP_INT_MAX;
+    }
+
     public function index()
     {
         $items = EvidenceItem::withCount('uploads')
             ->where('school_id', Auth::user()->school_id)
-            ->latest()
-            ->get();
+            ->get()
+            ->sort(function (EvidenceItem $first, EvidenceItem $second): int {
+                return $this->evidenceOrderNumber($first) <=> $this->evidenceOrderNumber($second)
+                    ?: strnatcasecmp($first->title, $second->title)
+                    ?: $first->id <=> $second->id;
+            })
+            ->values();
 
         return view('evidence.index', compact('items'));
     }
