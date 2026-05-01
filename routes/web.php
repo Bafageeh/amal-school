7e268c43c4b97fb230b4ebb8956955d844a5ae2b
Bafageeh <a.baf@live.com>
2026-05-02 02:07:49 +0300

    Add first password setup routes

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,11 @@ Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/set-password', [AuthController::class, 'showSetPassword'])->name('password.setup');
+    Route::post('/set-password', [AuthController::class, 'setPassword'])->name('password.setup.store');
+});
+
+Route::middleware(['auth', 'password.set'])->group(function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
     Route::resource('teachers', TeacherController::class)->except(['show', 'create']);
